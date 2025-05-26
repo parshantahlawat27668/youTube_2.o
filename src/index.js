06b05802062c43785c604d4debdbcc948912a386
parshant <ahlawatparshant57@gmail.com>
2025-05-26 18:19:34 +0530

    add package : cors, cookie-parser

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ config({ path: path.resolve(process.cwd(), '.env') });
 import app from "./app.js";
 import dbConnection from "./db/index.js";
 
-dbConnection();
-const PORT=process.env.PORT;
-app.listen(PORT,()=>{
-console.log("server running on port : ",PORT);
+const PORT=process.env.PORT || 8000;
+dbConnection()
+.then(()=>{
+    app.listen(PORT,()=>{
+    console.log("server running on port : ",PORT);
+    });
+})
+.catch((error)=>{
+    console.log("Database connection failed");
 });
